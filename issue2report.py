39c4e4ed6d6f1ba042a01aaf15852c2af336a659
pradyumna-muppirala <134854234+pradyumna-muppirala@users.noreply.github.com>
2023-09-28 20:30:29 +0530

    Static file name

--- issue2report.py
+++ issue2report.py
@@ -29,7 +29,7 @@ frozen_opts = {"{{DATE}}":date.today().strftime("%d/%m/%Y") }
 frozen_opts.update(customer)
 frozen_opts.update(template_frozen_opts)
 
-report_name = "{}-pentest-final-report-by-{}.pdf".format(frozen_opts["{{CUSTOMER}}"].lower().replace(" ","_"),frozen_opts["{{PENTESTER_NAME}}"].lower().replace(" ","_"))
+report_name = "OWASP_webGoat-pentest-final-report-by-{ASTICI.pdf"
 
 b = Build(output_dir,frozen_opts,p,g,t)
 
